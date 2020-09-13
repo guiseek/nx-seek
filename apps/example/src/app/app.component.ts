72bc1a4ce14e290ae3c1310ea6e8548d1b4be2d9
Guilherme Siquinelli <guiseek@gmail.com>
2020-09-13 01:09:38 -0300

    Initial commit

---
+++ apps/example/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-seek-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'example';
+}
